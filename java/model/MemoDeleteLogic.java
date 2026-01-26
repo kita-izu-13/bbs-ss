cfe6461a4f45273d5766416438fe76da74353ed1
kita-izu-13 <i.sakura315@gmail.com>
2026-01-26 16:07:07 +0900

    Delete java directory

--- java/model/MemoDeleteLogic.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-import java.util.List;
-
-public class MemoDeleteLogic {
-	public void delete(MemosDelete id, List<MemosDelete> deleteList) {
-		deleteList.add(0, id);	//先頭に追加
-	}
-}
